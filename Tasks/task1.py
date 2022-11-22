22c93c29fbd8c6a3b340f96db16cb26136721a79
KAUSHAL BHAVSAR <kaushalbhavsar004@gmail.com>
2022-11-22 11:33:49 +0530

    Task

---
+++ Tasks/task1.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter number 1 :"))
+num2 = int(input("Enter number 2 :"))
+if num1>0: 
+    print("Number 1 is positive")
+    if num1<0:
+        print("Number 2 is negative")
+if num2>0:
+    print("Number 2 is positive")
+
+else:
+    print("Number 2 is negative")
+
+
---
+++ Tasks/task2.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter 1st number"))
+num2 = int(input("enter 2nd number"))
+
+if num1%2 == 0:
+    print("number 1 is even")
+    
+else:
+    print("number 1 is odd")
+
+if num2%2 == 0:
+    print("number 2 is even")
+else:
+    print("number 2 is odd")
